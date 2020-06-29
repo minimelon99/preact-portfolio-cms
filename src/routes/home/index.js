e39ad6d509ee13cc850f3d3e9c1446c414ce2161
Amy Zhu <amy-achanstar@hotmail.com>
2020-06-28 21:49:38 -0700

    css updates

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,58 +1,43 @@
+/* eslint-disable lines-around-comment */
+/* eslint-disable indent */
 import { h } from "preact";
-import { useEffect } from 'preact/hooks';
+import { useEffect } from "preact/hooks";
+import { Link } from "preact-router";
+import { usePrerenderData } from "@preact/prerender-data-provider";
 import style from "./style";
 
 const Home = () => {
+  /**
+   * Netlify CMS's accept invite link land on home page.
+   * This redirection takes it to the right place(/admin).
+   */
 
-	/**
-	 * Netlify CMS's accept invite link land on home page.
-	 * This redirection takes it to the right place(/admin).
-	 */
+  useEffect(() => {
+    if (
+      window !== undefined &&
+      window.location.href.includes("#invite_token")
+    ) {
+      const { href } = window.location;
+      window.location.href = `${href.substring(
+        0,
+        href.indexOf("#")
+      )}admin${href.substring(href.indexOf("#"))}`;
+    }
+  }, []);
 
-	useEffect(() => {
-		if (window !== undefined && window.location.href.includes('#invite_token')) {
-			const { href } = window.location;
-			window.location.href= `${href.substring(0, href.indexOf('#'))}admin${href.substring(href.indexOf('#'))}`;
-		}
-	},[]);
-
-	return (
-		<div class={style.home}>
-			<div class={style.about}>
-				<div class={style.imageContainer}>
-					<div class={style.image} />
-				</div>
-				<div class={style.quote}>
-					<div class={style.details}>
-						Jane presents traditional spanish art with a modern twist. Her work is
-						excellent quality, her technique is brilliant and her love of the
-						subject matter shines through.
-					</div>
-					<div class={style.author}>- The Local Newspaper</div>
-				</div>
-			</div>
-			<div class={style.bio}>
-				<p class={style.bioleft}>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-					eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-					minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-					aliquip ex ea commodo consequat. Duis aute irure dolor in
-					reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-					pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-					culpa qui officia deserunt mollit anim id est laborum.
-				</p>
-				<p class={style.bioright}>
-					Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-					accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
-					ab illo inventore veritatis et quasi architecto beatae vitae dicta
-					sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-					aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
-					qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui
-					dolorem ipsum quia dolor sit amet, consectetur.
-				</p>
-			</div>
-		</div>
-	);
+  return (
+    <div class={style.home}>
+      <div class={style.about}>
+        <h1>Hi, I'm Amy</h1>
+        <h1>A product designer in Vancouver, BC</h1>
+        <div class={style.bar} />
+        <h2>
+          I deliver seamless user experience through problem-solving, empathy &
+          code. On the side, I like to draw.
+        </h2>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
